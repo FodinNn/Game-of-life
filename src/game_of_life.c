a2d0e15baee444995179162b4ce320ffcb879a6c
fodin <fodin11111@gmail.com>
2026-07-31 13:12:26 +0300

    add ncurses field rendering

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,25 +1,85 @@
 #include <ncurses.h>
+#include <stdio.h>
 
 #define ROWS 25
 #define COLUMNS 80
 
 typedef int GameField[ROWS][COLUMNS];
 
-static void fill_field_with_dead_cells(GameField field) {
+static int read_row(int row[COLUMNS]) {
+  int column;
+  int character;
+
+  for (column = 0; column < COLUMNS; column++) {
+    character = getchar();
+    if (character != '0' && character != '1') {
+      return 0;
+    }
+    row[column] = character - '0';
+  }
+
+  character = getchar();
+  if (character == '\r') {
+    character = getchar();
+    if (character != '\n') {
+      return 0;
+    }
+  }
+
+  return character == '\n' || character == EOF;
+}
+
+static int read_field(GameField field) {
+  int row;
+  int character;
+
+  for (row = 0; row < ROWS; row++) {
+    if (!read_row(field[row])) {
+      return 0;
+    }
+  }
+
+  character = getchar();
+  return character == EOF;
+}
+
+static int start_screen(void) {
+  if (initscr() == NULL) {
+    return 0;
+  }
+
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  curs_set(0);
+  return 1;
+}
+
+static void display_field(GameField field) {
   int row;
   int column;
 
+  erase();
   for (row = 0; row < ROWS; row++) {
     for (column = 0; column < COLUMNS; column++) {
-      field[row][column] = 0;
+      mvaddch(row, column, field[row][column] ? 'O' : ' ');
     }
   }
+  refresh();
 }
 
 int main(void) {
   GameField field;
 
-  fill_field_with_dead_cells(field);
+  if (!read_field(field) || freopen("/dev/tty", "r", stdin) == NULL) {
+    return 1;
+  }
+  if (!start_screen()) {
+    return 1;
+  }
 
+  display_field(field);
+  getch();
+  endwin();
   return 0;
 }
